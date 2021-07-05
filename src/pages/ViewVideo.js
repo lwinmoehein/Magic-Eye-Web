edaab9d5db30e69b2f13eaff4ec753679fe39fc9
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-07-05 07:09:37 +0630

    video-player :done

--- src/pages/ViewVideo.js
+++ src/pages/ViewVideo.js
@@ -1,23 +1,18 @@
 import "../styles/VideoViewerStyle.css";
 import React from "react";
 import { connect } from "react-redux";
-import { Media, Player, controls } from "react-media-player";
-const { PlayPause, MuteUnmute } = controls;
+import ReactPlayer from "react-player";
 
 function ViewVideo(props) {
   return (
-    <div className="videoViewerWrapper">
-      <Media>
-        <div className="media">
-          <div className="media-player">
-            <Player className="player" src={props.video.url} />
-          </div>
-        </div>
-        <div className="media-controls">
-          <PlayPause />
-          <MuteUnmute />
-        </div>
-      </Media>
+    <div className="player-wrapper">
+      <ReactPlayer
+        url={props.video.url}
+        className="react-player"
+        playing
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 }
