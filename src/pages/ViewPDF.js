6da29269d157167d794386a854648e20cab4fa62
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-07-25 10:08:36 +0630

    add:pdf view

--- src/pages/ViewPDF.js
+++ src/pages/ViewPDF.js
@@ -8,16 +8,39 @@ import { pdfjs } from "react-pdf";
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 import Download from "../components/Reusables/Download";
 import { setDownloadUrl, toggleProgress } from "../actions/index";
+import axios from "axios";
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 function ViewPDF(props) {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
+  let [downloadableVideoUrl, setDownloadableUrl] = useState("");
   useEffect(() => {
     if (!props.pdf) return;
+
     props.toggleProgress(true);
-    props.setDownloadUrl(props.pdf.url);
+    //extract absolute links
+    const extractorUrl = "https://www.oursecretworld.site/linkextractor.php";
+    let formData = new FormData();
+    formData.append("media_link", props.pdf.url);
+    axios({
+      method: "post",
+      url: extractorUrl,
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log("url:", response.data.absoluteLink);
+        setDownloadableUrl(response.data.absoluteLink);
+        props.toggleProgress(false);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+        props.toggleProgress(false);
+      });
   }, []);
 
   function onDocumentLoadSuccess({ numPages }) {
@@ -28,7 +51,29 @@ function ViewPDF(props) {
     props.toggleProgress(false);
   }
 
+  function openPDF() {
+    window.open(downloadableVideoUrl);
+    return false;
+  }
+
   if (!props.pdf) return <Redirect to="/" />;
+
+  let pdfView = <div></div>;
+  if (downloadableVideoUrl)
+    pdfView = (
+      <div className="pdfActionsWrapper">
+        <a
+          href={
+            "https://drive.google.com/viewerng/viewer?url=" +
+            downloadableVideoUrl
+          }
+          target="_blank"
+        >
+          View PDF
+        </a>
+        <a href={downloadableVideoUrl}>Download PDF</a>
+      </div>
+    );
 
   const PDFDocumentWrapper = styled.div`
     canvas {
@@ -37,22 +82,7 @@ function ViewPDF(props) {
     }
   `;
 
-  return (
-    <div>
-      <PDFDocumentWrapper>
-        <Document
-          file={{
-            url: props.pdf.url,
-          }}
-          loading=""
-          onLoadSuccess={onDocumentLoadSuccess}
-          onLoadError={onLoadError}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-      </PDFDocumentWrapper>
-    </div>
-  );
+  return <div>{pdfView}</div>;
 }
 
 const mapStateToProps = (state) => {
