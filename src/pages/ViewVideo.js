8ce20b916e2b6a63fa248bf54daac4190bb376c1
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-08-10 11:33:46 +0630

    change main player to youtube

--- src/pages/ViewVideo.js
+++ src/pages/ViewVideo.js
@@ -3,51 +3,72 @@ import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { toggleProgress } from "../actions/index";
-import VideoPlayer from "../components/Reusables/VideoPlayer";
 import axios from "axios";
-import NoData from "../components/Reusables/NoData";
+import YouTubePlayer from "../components/Reusables/YouTubePlayer";
+import VideoPlayer from "../components/Reusables/VideoPlayer";
+
+async function getMedialUrl(url) {
+  const extractorUrl =
+    "https://www.magiceyelearningcenter.com/linkextractor.php";
+  let formData = new FormData();
+  formData.append("media_link", url);
+  return axios({
+    method: "post",
+    url: extractorUrl,
+    data: formData,
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+}
 
 function ViewVideo(props) {
-  let [downloadableVideoUrl, setDownloadableVideoUrl] = useState("");
+  let [playableUrl, setPlayableUrl] = useState({});
+  let [mediaFirePlayerOptions, setMediaFirePlayerOptions] = useState({});
+
   useEffect(() => {
-    props.toggleProgress(true);
-    //extract absolute links
-    const extractorUrl = "https://www.oursecretworld.site/linkextractor.php";
-    let formData = new FormData();
-    formData.append("media_link", props.video.url);
-    axios({
-      method: "post",
-      url: extractorUrl,
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then(function (response) {
-        //handle success
-        console.log("url:", response.data.absoluteLink);
-        setDownloadableVideoUrl(response.data.absoluteLink);
-        props.toggleProgress(false);
-      })
-      .catch(function (response) {
-        //handle error
-        console.log(response);
-        props.toggleProgress(false);
+    //youtube
+    if (!props.video.url.includes("mediafire.com")) {
+      setPlayableUrl({
+        type: "YOUTUBE",
+        embedId: props.video.url.split(".be/")[1],
       });
+    } else {
+      const mediaFireUrl = getMedialUrl(
+        "https://www.mediafire.com/file/0xh3v78yzgbgtb0/The_Wanted_-_We_Own_The_Night_%2528Official_Video%2529.mp4/file"
+      )
+        .then(function (response) {
+          //handle success
+          const mediaFireUrl = response.data.absoluteLink;
+          if (mediaFireUrl) {
+            setPlayableUrl({
+              type: "MEDIA_FIRE",
+              downloadableUrl: mediaFireUrl,
+            });
+            console.log("playable", playableUrl);
+
+            const videoJsOptions = {
+              autoplay: true,
+              controls: true,
+              fluid: true,
+              aspectRatio: "16:7",
+              sources: [
+                {
+                  src: playableUrl.downloadableUrl,
+                  type: "video/mp4",
+                },
+              ],
+            };
+            setMediaFirePlayerOptions(videoJsOptions);
+            console.log("md options", mediaFirePlayerOptions);
+          }
+        })
+        .catch(function (response) {
+          return false;
+        });
+    }
   }, []);
 
   if (!props.video) return <Redirect to="/" />;
 
-  const videoJsOptions = {
-    autoplay: false,
-    controls: true,
-    fluid: true,
-    aspectRatio: "16:7",
-    sources: [
-      {
-        src: downloadableVideoUrl,
-        type: "video/mp4",
-      },
-    ],
-  };
   let videoView = (
     <div
       style={{
@@ -60,8 +81,12 @@ function ViewVideo(props) {
       Sorry,something wrong while extracting Video links !!!
     </div>
   );
-  if (downloadableVideoUrl)
-    videoView = <VideoPlayer className="player" {...videoJsOptions} />;
+
+  if ((playableUrl.type = "YOUTUBE") && playableUrl.embedId)
+    videoView = <YouTubePlayer playableUrl={playableUrl} />;
+
+  if ((playableUrl.type = "MEDIA_FIRE") && playableUrl.downloadableUrl)
+    videoView = <VideoPlayer className="player" {...mediaFirePlayerOptions} />;
 
   return <div className="videoViewerWrapper">{videoView}</div>;
 }
@@ -80,3 +105,42 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ViewVideo);
+
+// *** media fire player *** //
+// let [downloadableVideoUrl, setDownloadableVideoUrl] = useState("");
+// useEffect(() => {
+//   props.toggleProgress(true);
+//   //extract absolute links
+//   const extractorUrl = "https://www.oursecretworld.site/linkextractor.php";
+//   let formData = new FormData();
+//   formData.append("media_link", props.video.url);
+//   axios({
+//     method: "post",
+//     url: extractorUrl,
+//     data: formData,
+//     headers: { "Content-Type": "multipart/form-data" },
+//   })
+//     .then(function (response) {
+//       //handle success
+//       console.log("url:", response.data.absoluteLink);
+//       setDownloadableVideoUrl(response.data.absoluteLink);
+//       props.toggleProgress(false);
+//     })
+//     .catch(function (response) {
+//       //handle error
+//       console.log(response);
+//       props.toggleProgress(false);
+//     });
+// }, []);
+// const videoJsOptions = {
+//   autoplay: false,
+//   controls: true,
+//   fluid: true,
+//   aspectRatio: "16:7",
+//   sources: [
+//     {
+//       src: downloadableVideoUrl,
+//       type: "video/mp4",
+//     },
+//   ],
+// };
